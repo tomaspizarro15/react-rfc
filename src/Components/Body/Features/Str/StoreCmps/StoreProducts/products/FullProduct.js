f0cde5b84ac78f67c9fe2ac61a1eda6670e2507e
tomaspizarro15 <61084368+tomaspizarro15@users.noreply.github.com>
2020-06-10 15:59:34 -0300

    Axios GetByID added in FullProduct

--- src/Components/Body/Features/Str/StoreCmps/StoreProducts/products/FullProduct.js
+++ src/Components/Body/Features/Str/StoreCmps/StoreProducts/products/FullProduct.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 import './FullProduct.css';
-
+import * as Api from './../../../../../../Axios/Actions/Service/api';
 
 class FullProduct extends Component {
 
 
     state = {
-        url : "",
+        productID: "",
     }
 
-    componentDidMount(){
-        let url = this.props.match.url; 
+    componentDidMount() {
+        let url = this.props.match.url;
         let urls = url.split("/")
         let id = urls[3];
 
         console.log("[Products.js]", id)
-        this.setState({url : id})
-
+        this.setState({ productID: id })
+        Api.apiInstance.get("products/" + id).then(resp => {
+            console.log("Firebase Response", resp)
+        }).catch(error => console.log(error))
     }
-
-    componentDidUpdate(){
-    
-
-    }
-
     render() {
-        return(
-            <div className ="products_container">
+        console.log(`/products/${this.state.productID}`)
+
+        return (
+            <div className="full_product_container">
+                <div className="full_product">
+                    <div className ="product"></div>
+                    <div className ="product"></div>
+                </div>
             </div>
         )
     }
